d18c42b60a8b239977f910bc479e971ffb9f454b
Przemek Cichy <przemek.cichy@outlook.com>
2016-05-27 16:27:13 +0100

    Small cleanup of files, keybinding for inv swap

--- TypescriptMods/Misc/Misc.js
+++ TypescriptMods/Misc/Misc.js
@@ -1,5 +1,3 @@
-﻿//Swap items from inventory with pet inventory
-declare var Pet;
 function swap() {
     var swapNo = 0;
     function itterate(i) {
@@ -8,7 +6,8 @@ function swap() {
             if (players[0].temp.inventory[i].selected == true) {
                 i++;
                 console.log("SELECTED", i, swapNo);
-            } else if (swapNo < players[0].pet.chest.length) {
+            }
+            else if (swapNo < players[0].pet.chest.length) {
                 //swap  with first item in pet, max 16 times
                 Pet.menu_remove(0);
                 setTimeout(function () {
@@ -16,24 +15,27 @@ function swap() {
                 }, 130);
                 swapNo++;
                 console.log("swap", i, swapNo);
-            } else {
+            }
+            else {
                 console.log("break", i, swapNo);
                 i = players[0].temp.inventory.length;
             }
             var delay = Math.floor(Math.random() * (320 - 220 + 1)) + 220;
             setTimeout(function () {
                 itterate(i);
-            },delay);
+            }, delay);
             console.log("itteration", i, swapNo, delay);
         }
-    };
+    }
+    ;
     document.addEventListener("keydown", function (b) {
         //Key codes differ on different keyboards, although most keys are similar.
         // Number "2" is code 50, change it to whichever key you wish
         if (b.keyCode === 50) {
             swapNo = 0, itterate(0);
-        };
+        }
+        ;
     });
-
 }
 swap();
+//# sourceMappingURL=Misc.js.map
