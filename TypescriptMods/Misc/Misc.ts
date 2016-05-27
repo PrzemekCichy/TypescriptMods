05ff0cad0b030257406ec5bb1b1c0ecb3d38285c
Przemek Cichy <przemek.cichy@outlook.com>
2016-05-27 11:21:57 +0100

    Pet inventory swap

---
+++ TypescriptMods/Misc/Misc.ts
@@ -0,0 +1,28 @@
+﻿//Swap items from inventory with pet inventory
+declare var Pet;
+function swap() {
+    var swapNo = 0;
+    function itterate(i) {
+        if (i < players[0].temp.inventory.length) {
+            //set i to first non-equipped item
+            if (players[0].temp.inventory[i].selected == true) {
+                i++;
+                console.log("SELECTED", i, swapNo);
+            } else if (swapNo < players[0].pet.chest.length) {
+                //swap  with first item in pet, max 16 times
+                Pet.menu_add(i);
+                Pet.menu_remove(0);
+                swapNo++;
+                console.log("swap", i, swapNo);
+            } else {
+                console.log("break", i, swapNo);
+                i = players[0].temp.inventory.length;
+            }
+            setTimeout(function () {
+                itterate(i);
+            }, 173);
+            console.log("itteration", i, swapNo);
+        }
+    };
+    itterate(0);
+}
